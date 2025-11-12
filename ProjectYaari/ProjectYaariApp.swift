c040a8e8814cb830614c653973ad22cc8846f68b
Hritu Raj <sac3xhin@gmail.com>
2025-11-13 00:24:41 +0530

    Initial Commit

---
+++ ProjectYaari/ProjectYaariApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectYaariApp.swift
+//  ProjectYaari
+//
+//  Created by Hritu Raj on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectYaariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
